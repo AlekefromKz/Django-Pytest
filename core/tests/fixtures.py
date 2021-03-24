173c559494908b2ec0e0db0f4198a87098f556dd
alkydy <kydyrmin.kz@mail.ru>
2021-03-24 16:47:52 +0600

    moved fixtures to a separate file

---
+++ core/tests/fixtures.py
@@ -0,0 +1,28 @@
+import pytest
+from rest_framework.test import APIClient
+from core.factories import PrivilegeFactory, RoleFactory, PersonFactory
+
+
+@pytest.fixture
+def privilege(db):
+    return PrivilegeFactory()
+
+
+@pytest.fixture
+def role(db, privileges):
+    return RoleFactory(privileges=privileges)
+
+
+@pytest.fixture
+def person(db, role):
+    return PersonFactory(role=role)
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def privileges(db):
+    return [PrivilegeFactory(), PrivilegeFactory(), PrivilegeFactory()]
